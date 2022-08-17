93b86b117982bc0a2ca07e587699ca14ec67de6b
caijiawei1 <80509110+caijiawei1@users.noreply.github.com>
2022-08-17 16:19:56 +0800

    Create index.js
    Co-authored-by: Ve <bouquetrender@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,8 @@
-import {
-  preloadImages
-} from './utils';
-import {
-  Grid
-} from './grid';
+import { preloadImages } from "./utils";
+import { Grid } from "./grid";
 // Preload images then remove loader (loading class) from body
-preloadImages('.column__item-img').then(
-  () => {
-    document.body.classList.remove('loading');
-    // Initialize the grid
-    new Grid(document.querySelector('.columns'));
-  });
+preloadImages(".column__item-img").then(() => {
+  document.body.classList.remove("loading");
+  // Initialize the grid
+  new Grid(document.querySelector(".columns"));
+});
